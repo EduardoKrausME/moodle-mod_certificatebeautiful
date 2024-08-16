9b33e116d5b2de02331cb5d35ebd271f315295b5
Eduardo Kraus <kraus@eduardokraus.com>
2024-08-16 19:49:33 -0300

    BUG and Code Style

--- _editor/index.php
+++ _editor/index.php
@@ -1,3 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Editor for certificatebeautiful.
+ *
+ * @package     mod_certificatebeautiful
+ * @copyright   2024 Eduardo Kraus https://eduardokraus.com/
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../../config.php');
+require_login();
+$context = context_system::instance();
+
+require_capability('mod/certificatebeautiful:addinstance', $context);
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -22,12 +52,6 @@
     <script src="js/plugins/grapesjs-style-border.js"></script>
 
     <?php
-
-    require_once('../../../config.php');
-    require_login();
-    $context = context_system::instance();
-    require_capability('mod/certificatebeautiful:addinstance', $context);
-
     require_once("{$CFG->dirroot}/mod/certificatebeautiful/classes/local/fonts/font_util.php");
     $fontList = \mod_certificatebeautiful\local\fonts\font_util::mpdf_list_fonts();
 
