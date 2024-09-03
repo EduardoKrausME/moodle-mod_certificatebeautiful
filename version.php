aa67dd4ca1dff25fea53fe44bf037e2674ca56ac
Eduardo Kraus <kraus@eduardokraus.com>
2024-09-03 20:01:40 -0300

    V 2.1.13

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024081800;
-$plugin->release = '2.1.12';
+$plugin->version = 2024090300;
+$plugin->release = '2.1.13';
 $plugin->requires = 2021041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_certificatebeautiful';
