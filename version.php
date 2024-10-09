fda467519690412d774ada37231694c5126792b7
Eduardo Kraus <kraus@eduardokraus.com>
2024-10-09 10:26:17 -0300

    V 2.1.15

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2024090301;
-$plugin->release = '2.1.14';
+$plugin->version = 2024100900;
+$plugin->release = '2.1.15';
 $plugin->requires = 2021041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_certificatebeautiful';
