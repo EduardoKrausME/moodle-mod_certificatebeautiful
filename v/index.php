38e8e7a33a2f1f46bf69acd6b2b5f244a0344545
Eduardo Kraus <kraus@eduardokraus.com>
2024-10-22 15:05:06 -0300

    phpcs

--- v/index.php
+++ v/index.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * phpcs:disable moodle.Files.RequireLogin.Missing
+ *
  * Validate certificate instance.
  *
  * @package     mod_certificatebeautiful
