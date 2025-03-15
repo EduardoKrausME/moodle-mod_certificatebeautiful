8a88b1c57ccab50ff752f80cba8b619a68524c31
Eduardo Kraus <kraus@eduardokraus.com>
2025-03-15 11:40:43 -0300

    Rename path

--- reports.php
+++ reports.php
@@ -17,9 +17,9 @@
 /**
  * Report for certificatebeautiful.
  *
- * @package     mod_certificatebeautiful
- * @copyright   2024 Eduardo Kraus https://eduardokraus.com/
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   mod_certificatebeautiful
+ * @copyright 2025 Eduardo Kraus https://eduardokraus.com/
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require_once('../../config.php');
