b02917a97821a697c0dba79149d8f92ef7111a3a
Eduardo Kraus <kraus@eduardokraus.com>
2025-08-29 15:02:43 -0300

    copyright

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  * index file
  *
  * @package   mod_certificatebeautiful
- * @copyright 2025 Eduardo kraus (http://eduardokraus.com)
+ * @copyright 2025 Eduardo Kraus {@link https://eduardokraus.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
