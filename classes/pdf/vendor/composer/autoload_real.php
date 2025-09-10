f130ccaa992a4230254da915ec8456dd72195781
Eduardo Kraus <kraus@eduardokraus.com>
2025-09-10 12:51:50 -0300

    platform_check.php

--- classes/pdf/vendor/composer/autoload_real.php
+++ classes/pdf/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit5ce13f389270716aa74fac6c4293dda4
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit5ce13f389270716aa74fac6c4293dda4', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5ce13f389270716aa74fac6c4293dda4', 'loadClassLoader'));
