404f9041c26480f23c07c7d678e1ee68c4fed9a8
Eduardo Kraus <kraus@eduardokraus.com>
2025-09-10 15:17:59 -0300

    V 3.1.7

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2025091100;
-$plugin->release = "3.1.6";
+7$plugin->release = "3.1.7";
 $plugin->requires = 2021041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "mod_certificatebeautiful";
